641141a193575bbd78ccc0b4613784e86977e35d
YuriQuest <162909475+YuriQuest@users.noreply.github.com>
2024-11-22 13:12:40 -0300

    ⬆️ Update: second challenge

--- src/cod2.js
+++ src/cod2.js
@@ -1,3 +1,13 @@
-public class cod2 {
-    
-}
+// Desafios JavaScript na DIO têm funções "gets" e "print" acessíveis globalmente:
+//- "gets" : lê UMA linha com dado(s) de entrada (inputs) do usuário;
+//- "print" : imprime um texto de saída (output), pulando linha.
+
+const cor = gets();
+const tamanhoFonte = gets();
+const nomeBrinquedo = gets();
+
+// TODO: Construa a tag <h1> com o estilo CSS, completando com as variáveis representando cada estilização
+const resultado = `<h1 style="color: ${}; font-size: ${};">${nomeBrinquedo}</h1>`;
+
+// Exibindo o resultado no console
+print(resultado);
